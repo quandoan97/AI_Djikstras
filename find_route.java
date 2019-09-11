e669988807e8b7234704178af2611b17cda597fb
unknown <qdoan97@gmail.com>
2019-09-11 15:41:04 -0500

    added a file reader to take in input

--- find_route.java
+++ find_route.java
@@ -1,5 +1,17 @@
+import java.io.BufferedReader;
 import java.util.*;
 
 public class find_route{
-    
+    public static void main(String args[]){
+        String source = args[1];
+        String endpoint =  args[2];
+
+        File file = new File(args[0]);
+        BufferedReader buff = new BufferedReader(new FileReader(file));
+        String line;
+
+        while( line = buff.readLine() != null ){
+            String[] token = line.split(" ");
+        }
+    }
 }
