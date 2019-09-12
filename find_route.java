0f5081c76408d899577277e6479a97579fac1dd0
unknown <qdoan97@gmail.com>
2019-09-12 00:23:08 -0500

    Created the graph for all connecting nodes

--- find_route.java
+++ find_route.java
@@ -1,33 +1,61 @@
 import java.io.BufferedReader;
 import java.util.*;
+import java.io.*;
 
 public class find_route{
 
-    Node vertex{
-        String destination_city;
+    public static class vertexNode{
+        String cityName;
         int cost;
+
+        public vertexNode(String s, int c){
+            this.cityName = s;
+            this.cost = c;
+        }
     }
 
-    public static void main(String args[]){
+    public static void main(String args[])throws Exception {
         String source = args[1];
         String endpoint =  args[2];
 
-        
-
-        File file = new File(args[0]);
-        BufferedReader buff = new BufferedReader(new FileReader(file));
-        String line;
-
-        //reading in the file and parsing through to see the starting city and the cost to the next city
-        while( line = buff.readLine() != null ){
-            String[] tokens = line.split(" ");
-            if(tokens[2] == "INPUT"){
-                break;
+        HashMap<String, List<vertexNode>> graph = new HashMap<>();
+        List<vertexNode> value;
+            File file = new File(args[0]);
+            BufferedReader buff;
+            try{
+                buff = new BufferedReader(new FileReader(file));
+            }catch(FileNotFoundException e){
+                System.out.println("No file found");
+                return;
             }
-            String cityOne = tokens[0];
-            String cityTwo = tokens[1];
-            int cost = tokens[2];
+            String line;
 
-        }
+            //reading in the file and parsing through to see the starting city and the cost to the next city
+            //populates the graph
+            while( (line = buff.readLine()) != null ){
+                String[] tokens = line.split(" ");
+                if(tokens[2].equals("INPUT")){
+                    break;
+                }
+                String cityOne = tokens[0];
+                String cityTwo = tokens[1];
+                int cost = Integer.parseInt(tokens[2]);                
+                
+                value = graph.getOrDefault(cityOne, new ArrayList<vertexNode>());
+                value.add(new vertexNode(cityTwo, cost));
+                graph.put(cityOne, value);
+
+                value = graph.getOrDefault(cityTwo, new ArrayList<vertexNode>());
+                value.add(new vertexNode(cityOne, cost));
+                graph.put(cityTwo, value);
+            }
+            buff.close();
+
+            for(Map.Entry<String, List<vertexNode>> entry: graph.entrySet()){
+                System.out.printf("%s\n", entry.getKey());
+                for(vertexNode node: entry.getValue()){
+                    System.out.printf("Node: %s %d\n", node.cityName, node.cost);
+                }
+            }
     }
 }
