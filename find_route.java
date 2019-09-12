a3b2dda90e6cad5dea75b27673f3c276d2844af7
quandoan97 <qdoan97@gmail.com>
2019-09-11 20:15:32 -0500

    Parsed the file

--- find_route.java
+++ find_route.java
@@ -2,16 +2,32 @@ import java.io.BufferedReader;
 import java.util.*;
 
 public class find_route{
+
+    Node vertex{
+        String destination_city;
+        int cost;
+    }
+
     public static void main(String args[]){
         String source = args[1];
         String endpoint =  args[2];
+
+        
 
         File file = new File(args[0]);
         BufferedReader buff = new BufferedReader(new FileReader(file));
         String line;
 
+        //reading in the file and parsing through to see the starting city and the cost to the next city
         while( line = buff.readLine() != null ){
-            String[] token = line.split(" ");
+            String[] tokens = line.split(" ");
+            if(tokens[2] == "INPUT"){
+                break;
+            }
+            String cityOne = tokens[0];
+            String cityTwo = tokens[1];
+            int cost = tokens[2];
+
         }
     }
 }
